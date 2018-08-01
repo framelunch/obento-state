2d5ae6d7b0edabd7375d4f1f33c480561c2833d4
Yuichi Ikeda <ikeda@framelunch.jp>
2018-08-01 12:46:35 +0900

    v1.0.0

--- index.js
+++ index.js
@@ -1,51 +1,119 @@
+var Data = function(value) {
+  this.data = Data.make(value) || '';
+  this.str = '/' + this.data;
+  this.array = !this.data ? [''] : this.data.split('/');
+};
+Data.make = function(val) {
+  return val ? val.replace(/^\/|\/$/, '') : val;
+};
+Data.extract = function(arr1, arr2) {
+  var arr = [];
+  var l = arr1.length;
+  var i, a, b;
+  for (i = 0; i < l; i++) {
+    a = arr1.slice(0, i).join('/');
+    b = arr2.slice(0, i).join('/');
+    if (a + arr1[i] === b + arr2[i]) continue;
+
+    arr.push(arr1[i]);
+  }
+  return arr;
+};
+Data.isEq = function(data1, data2) {
+  return Data.make(data1) === Data.make(data2);
+};
+
 var Cp = require('obento-cp');
 var loop = require('obento-loop');
-
-var Data = function(value) {
-  this.data = this.make(value);
-  this.str = `/${this.data}`;
-  this.array = this.data === '' ? [''] : this.data.split('/');
-};
-Data.prototype = {
-  make: function() { },
-  compare: function() { },
-  eq: function() { }
-};
-
-var _set = function(prefix, items, pool, cps, arg) {
-
-};
 var State = function(def) {
+  this.pool = [];
+  this.active = false;
+  this.w = 0;
+  this.current = new Data(def || '');
+  this.cps = { 'state:start': new Cp(), 'state:update': new Cp(), 'state:end': new Cp() };
+  this.ite = 0;
 
+  var _this = this;
+  this.loop = function() {
+    _this._loop();
+  };
 };
-State.prototype = {
-  change: function(v, arg) {
+State.set = function(prefix, items, pool, cps, arg) {
+  for (var i = 0, l = items.length, name; i < l; i++) {
+    name = items[i];
+    pool.push({ prefix: prefix, name: name, arg: arg });
 
-  },
-  _change: function(v, arg) {
-
-  },
-  _loop: function() {
-
-  },
-  prev: function() {
-
-  },
-  next: function() {
-
-  },
-  wait: function() {
-
-  },
-  notify: function() {
-
-  },
-  listen: function(key, func) {
-
-  },
-  clear: function(key, func) {
-
+    if (!cps[(name = prefix + name)]) {
+      cps[name] = new Cp();
+    }
   }
 };
 
+State.prototype = {
+  change: function(val, arg) {
+    if (Data.isEq(this.current.data, val)) {
+      return false;
+    }
+
+    var data = new Data(val);
+    State.set(
+      'remove:',
+      Data.extract(this.current.array, data.array).reverse(),
+      this.pool,
+      this.cps,
+      arg
+    );
+    State.set(
+      'add:',
+      Data.extract(data.array, this.current.array),
+      this.pool,
+      this.cps,
+      arg
+    );
+
+    this.current = data;
+
+    if (!this.active) {
+      this.active = true;
+      this.cps['state:start'].update();
+      loop.add(this.loop);
+    }
+
+    this.cps['state:update'].update([this.current]);
+    return true;
+  },
+  _loop: function() {
+    if (this.w) return;
+
+    if (this.pool.length === 0) {
+      this.active = false;
+      this.cps['state:end'].update();
+      loop.remove(this.loop);
+      return;
+    }
+
+    var o = this.pool[0];
+    this.cps[o.prefix + o.name].update(o.arg);
+    this.pool.shift();
+  },
+  wait: function() {
+    ++this.w;
+  },
+  notify: function() {
+    --this.w;
+  },
+  listen: function(key, func) {
+    var c = this.cps[key];
+    if (!c) c = this.cps[key] = new Cp();
+    c.add(func);
+    return func;
+  },
+  clear: function(key, func) {
+    var c = this.cps[key];
+    if (!c) return;
+    c.remove(func);
+  },
+};
+
+State.Data = Data;
 module.exports = State;
--- index.test.js
+++ index.test.js
@@ -1,11 +1,49 @@
 const State = require('./index');
-const state = new State();
 
-it('loop basic', done => {
-  const mock = jest.fn();
+it('Data', done => {
+  const { make, extract, isEq } = State.Data;
+  expect(make('a/b')).toBe('a/b');
+  expect(make('/a/b')).toBe('a/b');
+  expect(make('a/b/')).toBe('a/b');
+  expect(make('/a/b/')).toBe('a/b');
+
+  expect(extract(['a', 'b', 'c'], ['a', 'b', 'e'])).toEqual(['e']);
+  expect(extract(['a', 'b', 'c'], ['a', 'd', 'e'])).toEqual(['d', 'e']);
+  expect(extract(['a', 'b', 'c'], ['d', 'b', 'c'])).toEqual(['d', 'b', 'c']);
+
+  expect(isEq('a/b', '/a/b')).toBe(true);
+  expect(isEq('a/b', 'a/b/')).toBe(true);
+  expect(isEq('a/b', '/a/b/')).toBe(true);
+  expect(isEq('a/b', '/a/b/c')).toBe(false);
+
+  done();
+});
+
+it('State', done => {
+  const state = new State();
+  const mock1 = jest.fn();
+  const mock2 = jest.fn();
 
   expect(state.listen).toBeInstanceOf(Function);
   expect(state.wait).toBeInstanceOf(Function);
 
-  done();
+  state.listen('add:a', val => console.log('add:a', val));
+  state.listen('add:b', val => console.log('add:b', val));
+  state.listen('add:c', val => console.log('add:c', val));
+  state.listen('remove:b', val => {
+    state.wait();
+    setTimeout(() => {
+      console.log('remove:b', val)
+      state.notify();
+    }, 1000);
+  });
+
+  state.listen('state:start', current => console.log('start'));
+  state.listen('state:update', current => console.log('update', current));
+  state.change('/a/b', [123]);
+  state.change('/a/c', [123]);
+
+  setTimeout(() => {
+    done();
+  }, 2000);
 });
